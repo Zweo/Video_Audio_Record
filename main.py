fab3c2c528b3f5bc75462a5b19f8878d41c2abfb
Zweo <ExZhouWei@outlook.com>
2019-10-30 14:32:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,10 @@ import os
 import subprocess
 import time
 
-from selenium import webdriver
-
 from video_audio_cap import SoundRecThread, VideoCapThread
 
 if __name__ == "__main__":
-    url = 'https://github.com/Zweo/Video_Audio_Record'
     output_path = ''
-    option = webdriver.ChromeOptions()  # 打开浏览器
-    option.add_argument(  # 设置成用户自己的数据目录
-        '--user-data-dir=C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data'
-    )
-    driver = webdriver.Chrome(options=option)
-    driver.get(url)
-
     avi_file = output_path + 'tmp.avi'
     wav_file = output_path + 'tmp.wav'
     t1 = VideoCapThread(avi_file)
