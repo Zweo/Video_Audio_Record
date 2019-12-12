15149d30c23c12a15b74b45ed578873eed91e38f
Wei Zhou <ExZhouWei@outlook.com>
2019-12-12 22:38:41 +0800

    添加一个GUI，FFmpeg 添加转换多个音视频的功能

--- gui.py
+++ gui.py
@@ -1,22 +1,47 @@
+'''
+github:Zweo
+2019.12.12
+'''
+import datetime
+import os
 import sys
+import threading
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QEvent, Qt
 from PyQt5.QtGui import QCursor
-from PyQt5.QtCore import Qt, QEvent
+
+from video_audio_cap import FFmpegThread, SoundRecThread, VideoCapThread
 
 SCREEN_WEIGHT = 1920 * 2
 SCREEN_HEIGHT = 1080
 WINDOW_WEIGHT = 180
 WINDOW_HEIGHT = 50
+RESULT_PATH = './res'  # 存储录屏和录音的位置
+
+
+def create_dirs():
+    if not os.path.exists(RESULT_PATH):
+        os.mkdir(RESULT_PATH)
+    video_path = os.path.join(RESULT_PATH, 'video')
+    sound_path = os.path.join(RESULT_PATH, 'sound')
+    if not os.path.exists(video_path):
+        os.mkdir(video_path)
+    if not os.path.exists(sound_path):
+        os.mkdir(sound_path)
+    return video_path, sound_path
 
 
 class qt_window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.press_start = True
+        self.start_pressed = False
+        self.file_path = None
+        self.dragPosition = QtCore.QPoint(0, 0)
+        self.video_path, self.sound_path = create_dirs()
 
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint
-                            | QtCore.Qt.WindowStaysOnTopHint
-                            | QtCore.Qt.Tool)  # 去掉标题栏
+                            | QtCore.Qt.Tool
+                            | QtCore.Qt.WindowStaysOnTopHint)  # 去掉标题栏
         # 控件布局
         self.button_start_pause = QtWidgets.QPushButton(u"开始")
         self.button_stop = QtWidgets.QPushButton(u"结束")
@@ -50,15 +75,34 @@ class qt_window(QtWidgets.QWidget):
         self.button_stop.clicked.connect(self.stop)
 
     def start_pause(self):
-        if self.press_start:
+        self.start_pressed = not self.start_pressed
+        if self.start_pressed:
             self.button_start_pause.setText('暂停')
-            self.press_start = False
+            now = str(datetime.datetime.now()).split('.')[0]
+            now = now.replace(' ', '_').replace(':', '.')
+            self.file_path = {
+                'video': os.path.join(self.video_path, now + '.avi'),
+                'sound': os.path.join(self.sound_path, now + '.wav'),
+                'output': os.path.join(RESULT_PATH, now + '.mp4')
+            }
+            self.video_recorder = VideoCapThread(self.file_path['video'])
+            self.sound_recorder = SoundRecThread(self.file_path['sound'])
+            self.video_recorder.start()
+            self.sound_recorder.start()
         else:
+            self.video_recorder.stoprecord()
+            self.sound_recorder.stoprecord()
             self.button_start_pause.setText('开始')
-            self.press_start = True
 
     def stop(self):
-        exit(0)
+        self.button_start_pause.setText('开始')
+        self.start_pressed = False
+        if self.file_path and os.path.exists(self.file_path['video']):
+            self.video_recorder.stoprecord()
+            self.sound_recorder.stoprecord()
+            ffmpeg_th = FFmpegThread(self.video_path, self.sound_path,
+                                     self.file_path['output'])
+            ffmpeg_th.start()
 
     def enterEvent(self, event):
         print('enter')
@@ -77,11 +121,8 @@ class qt_window(QtWidgets.QWidget):
 
     def mouseMoveEvent(self, event):
         if event.buttons() == Qt.LeftButton:
-            try:
-                self.move(event.globalPos() - self.dragPosition)
-                event.accept()
-            except:
-                print('Error:Move before Press')
+            self.move(event.globalPos() - self.dragPosition)
+            event.accept()
 
     def mouseReleaseEvent(self, event):
         self.hide_or_show('hide', event)
@@ -104,6 +145,20 @@ class qt_window(QtWidgets.QWidget):
             elif pos.x() <= 2:  # 左侧隐藏
                 self.move(QtCore.QPoint(2 - WINDOW_WEIGHT, pos.y()))
                 event.accept()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.stop()  # 开始转换
+            # self.showMinimized()
+            self.hide()
+            while True:
+                nums = threading.activeCount()
+                if nums == 7:
+                    exit(0)
+        elif event.key() == Qt.Key_F6:
+            self.start_pause()
+        elif event.key() == Qt.Key_F7:
+            self.stop()
 
 
 if __name__ == '__main__':
--- video_audio_cap.py
+++ video_audio_cap.py
@@ -1,9 +1,10 @@
-# python + opencv 实现屏幕录制
+# 录音录屏，合成
 import queue
 import sys
 import threading
 import wave
-
+import subprocess
+import os
 from cv2 import cv2
 import numpy as np
 import sounddevice as sd
@@ -49,7 +50,7 @@ class AudioRecThread(threading.Thread):
 
     def run(self):
         audio = PyAudio()
-        wavfile = wave.open(self.audiofile, 'wb')
+        wavfile = wave.open(self.audiofile, 'ab')
         wavfile.setnchannels(self.channels)
         wavfile.setsampwidth(audio.get_sample_size(self.format))
         wavfile.setframerate(self.rate)
@@ -100,3 +101,60 @@ class SoundRecThread(threading.Thread):
 
     def stoprecord(self):
         self.bRecord = False
+
+
+'''*******************************FFmpeg音视频合成*******************************'''
+
+
+class FFmpegThread(threading.Thread):
+    def __init__(self, avi_file, wav_file, mp4_file):
+        threading.Thread.__init__(self)
+        self.avi_file = avi_file
+        self.wav_file = wav_file
+        self.mp4_file = mp4_file
+        self.mode = 'dir' if os.path.isdir(self.avi_file) else 'file'
+
+    def combine_to_mp4(self, avi_file, wav_file, mp4_file):
+        subprocess.call('ffmpeg -i {} -i {} -strict -2 -f mp4 {}'.format(
+            avi_file, wav_file, mp4_file))
+        os.remove(avi_file)
+        os.remove(wav_file)
+
+    def run(self):
+        print('FFmpeg Start ……')
+        if self.mode == 'file':
+            self.combine_to_mp4(self.avi_file, self.wav_file, self.mp4_file)
+        elif self.mode == 'dir':
+            avi_files = os.listdir(self.avi_file)
+            wav_files = os.listdir(self.wav_file)
+            mp4_files = []
+            file_num = len(avi_files)
+            for i in range(file_num):
+                avi_file = os.path.join(self.avi_file, avi_files[i])
+                wav_file = os.path.join(self.wav_file, wav_files[i])
+                if file_num == 1:
+                    self.combine_to_mp4(avi_file, wav_file, self.mp4_file)
+                    return
+                mp4_file = self.mp4_file + '_{}.mp4'.format(i)
+                mp4_files.append(mp4_file)
+                self.combine_to_mp4(avi_file, wav_file, mp4_file)
+            self.ts_to_mp4(mp4_files)
+
+    def ts_to_mp4(self, files):
+        ts_files = []
+        for file in files:
+            ts_file = file[:-3] + 'ts'
+            ts_files.append(ts_file)
+            command = 'ffmpeg -i {} -c copy -vbsf h264_mp4toannexb {}'
+            command = command.format(file, ts_file)
+            subprocess.call(command)
+        input_files = '|'.join(ts_files)
+        command = 'ffmpeg.exe -i "concat:{}" -c copy -absf aac_adtstoasc {}'
+        command = command.format(input_files, self.mp4_file)
+        subprocess.call(command)
+        for item in files:
+            os.remove(item)
+        for item in ts_files:
+            os.remove(item)
+
+        print('FFmpeg Finish ……')
